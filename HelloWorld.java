02431b1b5e7aa0bcbc2acc9c420292b811e73757
linfeng <root_vip128@163.com>
2018-06-25 09:01:59 +0800

    案例

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package cn.tedu.day01.demo;
+
+public class HelloWorld {
+
+	/*
+	 * 注释1
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		//写出意思
+		System.out.println("你好");
+	}
+
+}
